fdded5145133d93d09fcda4c954f25dc0bed95b9
Brandon-Chan03 <Brandon.chan0216@gmail.com>
2023-11-29 13:55:02 -0600

    still trying to fix this video not working

--- src/components/home/HomeHero.js
+++ src/components/home/HomeHero.js
@@ -5,7 +5,10 @@ import video from "../../../public/video.mp4";
 const HomeHero = () => {
   return (
     <div className="flex justify-center items-center w-full relative">
-      <video autoPlay loop muted className="w-full h-auto" src={video} />
+      {/* <video autoPlay loop muted className="w-full h-auto" src={"/public/video.mp4"} /> */}
+      <video autoPlay loop muted className="w-full h-auto">
+        <source src={require("../../../public/video.mp4")} type="video/mp4" />
+      </video>
       <div className="mt-[calc(100%/4)] absolute inset-0 left-[calc(100%/32)] flex items-center justify-center">
         <Link className="w-[calc(100%/4)] text-center text-white py-1 text-2xl bg-black rounded-[9px] hover:text-black hover:bg-white" href="/downloads">
           Play for Free
